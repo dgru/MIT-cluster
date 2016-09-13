0991773f22f367720c947c14db630059f25ddd09
Danielle Gruen <dgru@users.noreply.github.com>
2016-09-13 15:00:33 -0400

    Update submit_muscle_standard.sh

--- submit_muscle_standard.sh
+++ submit_muscle_standard.sh
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=20				# CPUs per task
 #SBATCH --mem=32000					# 1000 is default
 #SBATCH -t 0-08:00:00 					# maxtime:  DAY-Hour:Min:Sec, will stop after this time!
-#SBATCH -J raxml_DSG 					# Job Name (change this for your run)
-#SBATCH -o raxml_DSG.out 				# Job Name; outputs a test file (change this for your run)
+#SBATCH -J muscle_DSG 					# Job Name (change this for your run)
+#SBATCH -o muscle_DSG.out 				# Job Name; outputs a test file (change this for your run)
 
 # module setup
 .  /etc/profile.d/modules.sh 				# load modules
@@ -21,6 +21,6 @@ muscle -in inputfilename -out outputfilename
 	
 
 # additional notes
-# to run this job submission script type "sbatch submit_raxml.sh" (or whatever you name the file)
-# tail -f raxml_DSG.out 				# run this line outside of script from cluster to look at output of script while its running
+# to run this job submission script type "sbatch submit_muscle_standard.sh" (or whatever you name the file)
+# tail -f muscle_DSG.out 				# run this line outside of script from cluster to look at output of script while its running
 # squeue -u dgruen 					# type this line to check on if your jobs are running or waiting to run on the queue (use your username)
